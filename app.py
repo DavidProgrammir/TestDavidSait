0f4b8724041f9f610b457d64c2c3815ece7a82da
Давид <xristosvockrec@gmail.com>
2023-06-04 19:12:52 +1000

    test

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect, send_from_directory
+import sqlite3
+#from registration import regisration_user_in_Database()
+def regisration_user_in_Database(userid,name,second_name,birthday,login,email,password):
+    path = r'/home/nubuk/Programming/Testsait/TestSait_DataBase.db'
+    with sqlite3.connect(path) as connection:
+        cur = connection.cursor()
+
+        cur.execute(f"""CREATE TABLE IF NOT EXISTS users(
+        userid INT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        second_name VARCHAR(255),
+        birthday Date,
+        login VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL),
+        password TEXT;
+        """)
+
+        cur.execute(f"""INSERT INTO users(userid, name, second_name, birthday, login, email, password) 
+            VALUES('{userid}', '{name}', '{second_name}', '{birthday}' '{login}' '{email}' '{password}');""")
+        connection.commit()
+        #all = cur.fetchall()
+
+        #cur.execute("""INSERT INTO users(userid, fname, lname, gender) 
+        #    VALUES('00001', 'Alex', 'Smith', 'man');""")
+
+def next_(id):
+    path = r'/home/nubuk/Programming/Testsait/TestSait_DataBase.db'
+    with sqlite3.connect(path) as connection:
+        cur = connection.cursor()
+
+        cur.execute("""SELECT MAX(userid) FROM users;""")
+        object_with_max_userid = cur.fetchall()
+        print(object_with_max_userid)
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/succes_registration')
+def succes_registration():
+    return render_template('succes_registration.html')
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration_sait():
+    if request.method == 'GET':
+        return render_template('registration.html')
+    name = request.form['name']
+    second_name = request.form['second_name']
+    birthday = request.form['birthday']
+    login = request.form['login']
+    email = request.form['email']
+    password = request.form['password']
+    regisration_user_in_Database(0, name, second_name, birthday, login, email, password)
+    #return render_template('succes_registration.html')
+    return redirect('/succes_registration')
+
+app.run()
+
+#path = r'/home/nubuk/Programming/Testsait/TestSait_DataBase.db'
+#with sqlite3.connect(path) as connection:
+#    cur = connection.cursor()
+#    cur.execute(f"""SELECT MAX(userid) FROM users;""")
+#    print(cur.fetchall())
---
+++ registration.py
@@ -0,0 +1,3 @@
+from flask import Flask, render_template, request, redirect, send_from_directory
+import sqlite3
+
